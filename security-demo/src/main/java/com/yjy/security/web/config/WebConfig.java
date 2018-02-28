dca96bf2fd835b302241f42d8ee2337cd9fec4e6
ustbyjy <ustbyjjy@163.com>
2018-02-28 23:58:43 +0800

    异步处理

--- security-demo/src/main/java/com/yjy/security/web/config/WebConfig.java
+++ security-demo/src/main/java/com/yjy/security/web/config/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -21,6 +22,16 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(timeInterceptor);
+    }
+
+    /**
+     * 注册异步请求
+     *
+     * @param configurer
+     */
+    @Override
+    public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
+        super.configureAsyncSupport(configurer);
     }
 
     @Bean
