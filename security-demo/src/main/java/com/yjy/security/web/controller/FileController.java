01541930681602fde1a9450f694da2debbf74344
ustbyjy <ustbyjjy@163.com>
2018-02-28 22:57:04 +0800

    Upload And Download

---
+++ security-demo/src/main/java/com/yjy/security/web/controller/FileController.java
@@ -0,0 +1,40 @@
+package com.yjy.security.web.controller;
+
+import com.yjy.security.dto.FileInfo;
+import org.apache.commons.io.IOUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
+@RestController
+@RequestMapping("/file")
+public class FileController {
+    private static final String path = "D:\\IdeaProjects\\security\\security-demo";
+
+    @PostMapping
+    public FileInfo upload(MultipartFile file) throws IOException {
+        System.out.println(file.getName());
+        System.out.println(file.getOriginalFilename());
+        System.out.println(file.getSize());
+
+        File localFile = new File(path, System.currentTimeMillis() + ".txt");
+        file.transferTo(localFile);
+
+        return new FileInfo(localFile.getAbsolutePath());
+    }
+
+    @GetMapping("/{id}")
+    public void download(@PathVariable String id, HttpServletResponse response) {
+        try (InputStream inputStream = new FileInputStream(new File(path, id + ".txt"));
+             OutputStream outputStream = response.getOutputStream()) {
+            response.setContentType("application/x-download");
+            response.addHeader("Content-Disposition", "attachment;filename=test.txt");
+            IOUtils.copy(inputStream, outputStream);
+            outputStream.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
